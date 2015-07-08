14d0549a5ea31ea8249b91bd6694588de3f094c1
John Wang <johnw@dmems.com>
2015-07-08 10:03:22 +0800

    Initial code

---
+++ dmc_actin/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OGO Process Improvements Module
+#    Copyright (C) 2012 OGO (<http://www.odoogo.com>).
+#
+##############################################################################
+{
+    'name': 'DM ACTIN',
+    'version': '1.0',
+    'category': 'Customization',
+    'author': 'DMEMS',
+    'website': 'www.dmems.com',
+    'description': """
+ACTIN Customization
+=====================
+
+* Feature1
+* Feature2
+
+    """,
+    'depends': ['base'],
+    'data':[],
+    'auto_install': False,
+    'installable': True
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
