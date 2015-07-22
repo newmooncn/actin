93223f85d476308f0ddc3430ff14ee673fe8b68e
John Wang <johnw@dmems.com>
2015-07-22 08:54:22 +0800

    Did improvements

--- dmc_actin/product.py
+++ dmc_actin/product.py
@@ -63,6 +63,6 @@ class product_product(osv.osv):
 		'seller_lead_time': fields.integer('Leadtime'),
 		'quote_validity': fields.integer('Quotation validity')
 	}
-	_defaults = {'type':'service'}
+#	_defaults = {'type':'service'}
 				
 product_product()
