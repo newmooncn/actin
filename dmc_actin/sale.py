281e7fd1742fd60577f90a9d404775ee608eecdf
John Wang <johnw@dmems.com>
2015-10-27 09:20:49 +0800

    Fix create PI from PO issue

--- dmc_actin/sale.py
+++ dmc_actin/sale.py
@@ -22,6 +22,9 @@
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 import openerp.addons.decimal_precision as dp
+import time
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
+
 class sale_order(osv.osv):
 	_inherit="sale.order"
 	_columns = {
