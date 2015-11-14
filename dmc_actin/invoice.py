2075bd428af6cb1ced7a913b31de839c7eb1e6dd
John Wang <johnw@dmems.com>
2015-11-14 23:44:53 +0800

    Improve the invoice field invisible by is service flag

--- dmc_actin/invoice.py
+++ dmc_actin/invoice.py
@@ -49,7 +49,7 @@ class account_invoice(models.Model):
 												('others','Others')),
 									string='Service type')		 
 	 
-	parent_id = fields.Many2one('account.invoice', 'Commercial Invoice', select=True)	
+	parent_id = fields.Many2one('account.invoice', 'Commercial Invoice', select=True, domain="[('is_service','=',False),('type','=','out_invoice')]")	
 	child_ids = fields.One2many('account.invoice', 'parent_id', 'Service Invoices')
 	
 	#fields for loading/discharge ports
@@ -94,4 +94,24 @@ class account_invoice(models.Model):
 			#result.append((inv.id, "%s %s" % (inv.number or TYPES[inv.type], inv.name or '')))
 			result.append((inv.id, inv.number or TYPES[inv.type]))
 		return result	
+	
+	@api.multi
+	def onchange_payment_term_date_invoice(self, payment_term_id, date_invoice):
+		resu = super(account_invoice, self).onchange_payment_term_date_invoice(payment_term_id, date_invoice)
+		#johnw, 2015/11/10, set date invoice to date due, it is the current date, ref super.onchange_payment_term_date_invoice() for detail.
+		if not payment_term_id:
+			resu['value']['date_invoice'] = resu['value']['date_due']
+		return resu
+	
+	@api.model
+	def name_search(self, name, args=None, operator='ilike', limit=100):
+		args = args or []
+		recs = self.browse()
+		if name:
+			recs = self.search([('number', '=', name)] + args, limit=limit)
+		if not recs:
+			#johnw, 2015/11/10, for the number, use 'operator'
+			#recs = self.search([('name', operator, name)] + args, limit=limit)
+			recs = self.search(['|',('name', operator, name),('number', operator , name)] + args, limit=limit)
+		return recs.name_get()	
 #po_super.STATE_SELECTION = STATE_SELECTION_PO	
